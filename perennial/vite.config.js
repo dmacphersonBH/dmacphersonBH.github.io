a497da47788adaa9fd67e6b05eef35adc3d6f84d
dmacphersonBH <dev.macpherson@gmail.com>
2026-03-26 09:18:43 -0400

    docs folder

--- perennial/vite.config.js
+++ perennial/vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/dmacphersonBH.github.io/'
+  base: '/perennial/', // Keep this! It's still necessary for sub-folder routing.
+  build: {
+    outDir: 'docs', // <-- Tells Vite to name the output folder "docs" instead of "dist"
+  }
 })
